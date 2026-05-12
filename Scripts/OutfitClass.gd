592268e80c4db998695e0eff2fa41d4497ae6a43
Saxinpoppy <exiong3712@gmail.com>
2026-05-11 23:27:09 -0400

    Added Clothing Managing
    Added Clothing managing logic, nested arrays for player clothing storage, and calculations for scoring.

---
+++ Scripts/OutfitClass.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+class_name Outfit
+
+@export var length : float
+@export var thickness : float
+@export var size : float
+@export var color : String
+@export var style : String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
